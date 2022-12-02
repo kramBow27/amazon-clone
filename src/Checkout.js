3a7e5a6e5998f32d1490f036ea5dc7d5da7be6f0
kramBow27 <kram.bow.instanceof@gmail.com>
2022-12-02 16:17:55 -0300

    before-flip

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import './Checkout.css';
 import Subtotal from './Subtotal';
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
+
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -15,6 +16,7 @@ function Checkout() {
           alt=""
         />
         <div>
+          <h3>Hello {user?.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
